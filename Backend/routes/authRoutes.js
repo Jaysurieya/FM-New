8b3a6a11e5a58880c1e240425b755b951dd96dd2
Jaysurieya <jaysurieyayuvaraja@gmail.com>
2025-12-17 11:23:08 +0530

    unwanted code removed in the backend

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../Controllers/authController');
 
-router.post('/signup', authController.signup);
-router.post('/login', authController.login);
 
 router.post("/google", authController.googleLogin);
 router.post("/email-auth", authController.emailAuth);
