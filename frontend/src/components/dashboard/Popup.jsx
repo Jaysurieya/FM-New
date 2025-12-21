eb5c22c2276bd72e879044303c045da22591ad35
Jaysurieya <jaysurieyayuvaraja@gmail.com>
2025-12-21 13:54:30 +0530

    testing

--- frontend/src/components/dashboard/Popup.jsx
+++ frontend/src/components/dashboard/Popup.jsx
@@ -131,7 +131,7 @@ function Popup({onClose, videoRef: externalVideoRef,onAddNutrition}) {
         }
 
         // Send to ML backend
-        const res = await fetch("https://fm-new.onrender.com/predict", {
+        const res = await fetch(" http://127.0.0.1:5001/predict", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ image: pixels }),
